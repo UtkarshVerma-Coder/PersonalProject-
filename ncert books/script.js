ef278e4a3ae50e04b3fedf51f0e3a126b29703d8
UtkarshVerma-Coder <utkarshverma001@outlook.com>
2022-03-27 00:09:54 +0530

    Added NCERT FOLEDER WITH CBSE BOOKS OPTION AND ABOUT PAGE

---
+++ ncert books/script.js
@@ -0,0 +1,8 @@
+
+// Hiding and Showing property of class 10
+function class10() {
+    document.getElementById("selectclass").style.display = 'block'; 
+    document.getElementById("class10").style.marginLeft = "80px";
+    document.getElementById("class9").style.display = 'none'; 
+}
+// Hiding and Showing property of class 10
