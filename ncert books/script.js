461c93727e7780f09a687ec284b762f1c5760898
UtkarshVerma-Coder <utkarshverma001@outlook.com>
2022-04-07 00:09:06 +0530

    A

--- ncert books/script.js
+++ ncert books/script.js
@@ -1,10 +1,17 @@
-
-// Hiding and Showing property of class 10
-function class10() {
-    document.getElementById("selectclass").style.display = 'block'; 
-    document.getElementById("class10").style.marginLeft = "80px";
-    document.getElementById("class9").style.display = 'none'; 
+function HD(){
+  document.getElementById("class9").style.display = 'none'; 
+  document.getElementById("SUB10VISIBLE").style.display = 'block'; 
+  document.getElementById("chap").style.display = 'none'; 
+  
 }
-// Hiding and Showing property of class 10
+
+document.getElementById("chap").style.display = 'flex';
+
+function math() {
+  document.getElementById("SUB10VISIBLE").style.display = 'none'; 
+  document.getElementById("chap").style.display = 'block';
+  
+}
+
 
 
